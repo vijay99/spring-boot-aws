d6982fc291b5c22fccd5dfcfbf468dc4a313719a
Kumar Vijay <vijay81238@gmail.com>
2023-06-17 21:52:19 +0530

    Initial commit

---
+++ spring-boot-ecs/src/main/java/com/example/springbootecs/SpringBootEcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-ecs/src/test/java/com/example/springbootecs/SpringBootEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
